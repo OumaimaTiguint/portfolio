e312bd6f400595a16cd69a5eec654fde503ecea2
OumaimaTiguint <oumaimatiguint@gmail.com>
2020-09-08 13:53:44 +0100

    updated

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import './projects.css';
-
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode } from '@fortawesome/fontawesome-free-solid';
 
 class Projects extends React.Component {
     projects= [
     { 
-        code: "https://github.com/OumaimaTiguint/Global-and-Hemispheric-Monthly-Means",
-        title: "Global and Hemispheric temperatures"
+        link: "https://mock-salon.netlify.app/",
+        code: "https://github.com/OumaimaTiguint/mock-salon",
+        title: "Hair Salon",
+        img: 'pictures/salon.png'
     }, 
     { 
-        code: "https://github.com/OumaimaTiguint/ISS-location",
-        title: "ISS Location"
+        link: "https://mock-bookstore.netlify.app/",
+        code: "https://github.com/OumaimaTiguint/bookstore",
+        title: "Bookstore",
+        img: 'pictures/bookstore.png'
     }, 
     { 
+        link: "https://qizz.netlify.app/",
         code: "https://github.com/OumaimaTiguint/React-quiz",
-        title: "Quizz"
+        title: "Quizz",
+        img: 'pictures/quizz.png'
     },
-    { 
-        code: "https://github.com/OumaimaTiguint/flappy-bird-ball-",
-        title: "Flappy bird"
+    {
+        link: "https://iss-location.netlify.app/",
+        code: "https://github.com/OumaimaTiguint/ISS-location",
+        title: "ISS Location",
+        img: 'pictures/iss.png'
     }
-
     ]
     render() {
         return (
@@ -32,12 +40,20 @@ class Projects extends React.Component {
                 <div className="projects">
                     { 
                         this.projects.map(project => 
-                        <div className="project" key={project.title}>
-                            <h3>{project.title} </h3>
-                            <button className="code" onClick={()=> window.open(project.code, "_blank")}>
-                                <FontAwesomeIcon icon={faCode} />
-                            </button>
-                        </div>)
+                            <Popup trigger={
+                                <div className="project" key={project.title}>
+                                    <h3>{project.title} </h3>
+                                    <button className="code" onClick={()=> window.open(project.code, "_blank")}>
+                                        <FontAwesomeIcon icon={faCode} />
+                                    </button>
+                                    <button className="link"  onClick={()=> window.open(project.link, "_blank")}>Check out the site</button>
+                                </div>
+                            } position="right center">
+                                <div className="popup">
+                                    <img alt={project.title} src={project.img}></img>
+                                </div>
+                            </Popup>
+                        )
                     }
                 </div>
             </div>
